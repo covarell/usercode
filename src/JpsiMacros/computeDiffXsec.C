27b117b907cfca636437a1986a539f18b5f88cd9
covarell <>
2010-09-22 13:23:01 +0000

    New binnings

--- src/JpsiMacros/computeDiffXsec.C
+++ src/JpsiMacros/computeDiffXsec.C
@@ -14,20 +14,31 @@
 
 void computeDiffXsec() {
 
-  static const unsigned int smallBins = 13;
-  static const unsigned int largeBins = 8;
-  int howManyBins[largeBins] = {1,2,1,3,2,1,2,1};
-  double binWidths[smallBins] = {2.,2.,2.,20.,1.,0.5,0.5,1.,1.,2.,2.,2.,20.};
-  double binWidths2[largeBins] = {2.,4.,20.,2.,2.,2.,4.,20.};
+  static const unsigned int smallBins = 33;
+  static const unsigned int largeBins = 15;
+  int howManyBins[largeBins] = {2,2,2,2,2,2,1,4,3,3,3,2,2,2,1};
+  double binWidths[smallBins] = {1.,1.,1.5,2.,2.,18.,1.5,1.,1.,1.,1.5,2.,20.,
+				 0.5,0.25,0.25,0.25,0.25,0.25,0.25,0.25,
+				 0.25,0.25,0.25,0.25,0.25,0.5,0.5,0.5,1.,
+				 1.5,2.,20.};
+  double binWidths2[largeBins] = {2.,3.5,20.,2.5,2.,3.5,20.,1.25,0.75,0.75,0.75,1.,2.,3.5,20.};
    
   // YIELDS AND ERRORS ("MIXED" POLARIZATION)
-  // double inclXsec[smallBins] = {34.622,15.951,8.514,0.662,191.050,375.838,344.348,199.419,111.534,55.972,14.289,7.019,0.322};
-  // double errstXsec[smallBins] = {2.971,0.936,0.498,0.035,11.849,34.427,38.920,8.197,12.335,2.455,0.690,0.440,0.024};
-  // double errsyXsec[smallBins] = {7.575,2.384,1.379,0.091,58.991,95.725,72.548,30.253,18.289,13.247,2.395,2.085,0.050};
-  // YIELDS AND ERRORS (NO POLARIZATION)
-  double inclXsec[smallBins] = {34.89,16.18,8.49,0.653,214.0,357.0,335.0,197.0,109.0,54.6,14.91,5.88,0.307};
-  double errstXsec[smallBins] = {2.50,0.84,0.45,0.031,14.0,34.0,20.0,8.0,4.0,3.1,0.64,0.34,0.024};
-  double errsyXsec[smallBins] = {6.00,2.33,1.35,0.097,66.0,90.0,70.0,29.0,17.0,10.0,2.61,1.00,0.048};
+  double inclXsec[smallBins] = {35.33,23.29,18.31,7.84,2.86,0.278,
+				55.04,37.95,23.62,13.22,6.12,2.21,0.145,
+				62.64,146.08,170.64,219.17,203.07,230.44,
+				194.60,200.09,157.40,155.64,123.94,118.03,
+				107.12,80.03,62.94,38.27,20.22,9.92,3.85,0.239};
+  double errstXsec[smallBins] = {2.31,1.38,0.71,0.28,0.13,0.011,
+				 4.92,2.33,1.19,0.65,0.24,0.11,0.008,
+				 4.37,10.47,10.28,12.18,9.87,11.05,
+				 9.69,10.14,31.46,9.66,5.98,5.52,
+				 4.80,17.81,2.32,1.19,0.72,0.39,0.17,0.012};
+  double errsyXsec[smallBins] = {9.33,5.72,2.93,1.20,0.57,0.044,
+				 14.55,9.14,3.56,1.64,0.79,0.30,0.022,
+				 15.03,49.32,62.07,73.25,69.14,73.25,
+				 50.51,43.32,30.94,28.03,21.03,19.93,
+				 16.33,12.23,8.40,5.43,2.81,1.28,0.49,0.031};
 
   // POLARIZATION FROM FILE
   double nopol[smallBins];
@@ -38,38 +49,50 @@ void computeDiffXsec() {
   double B409[smallBins];
   double B333[smallBins];
 
-  TFile f1("large_bins/acceptance_polarized.root");
+  TFile* accfile[3];
 
-  TH2F *Jpsi_non = (TH2F*)f1.Get("Jpsi_non");
-  TH2F *Jpsi_t_HXtheta = (TH2F*)f1.Get("Jpsi_t_HXtheta");
-  TH2F *Jpsi_l_HXtheta = (TH2F*)f1.Get("Jpsi_l_HXtheta");
-  TH2F *Jpsi_t_CStheta = (TH2F*)f1.Get("Jpsi_t_CStheta");
-  TH2F *Jpsi_l_CStheta = (TH2F*)f1.Get("Jpsi_l_CStheta");
-  TH2F *B_409 = (TH2F*)f1.Get("B_409");
-  TH2F *B_333 = (TH2F*)f1.Get("B_333");
+  accfile[0] = TFile::Open("accept/0._1.2_acceptance_polarized.root");
+  accfile[1] = TFile::Open("accept/1.2_1.6_acceptance_polarized.root");
+  accfile[2] = TFile::Open("accept/1.6_2.4_acceptance_polarized.root");
+
+  TH2F *Jpsi_non[3];
+  TH2F *Jpsi_t_HXtheta[3];
+  TH2F *Jpsi_l_HXtheta[3];
+  TH2F *Jpsi_t_CStheta[3];
+  TH2F *Jpsi_l_CStheta[3];
+  TH2F *B_409[3];
+  TH2F *B_333[3];
+
+  for (int i=0; i < 3; i++) {
+    Jpsi_non[i] = (TH2F*)(accfile[i]->Get("Jpsi_non"));
+    Jpsi_t_HXtheta[i] = (TH2F*)(accfile[i]->Get("Jpsi_t_HXtheta"));
+    Jpsi_l_HXtheta[i] = (TH2F*)(accfile[i]->Get("Jpsi_l_HXtheta"));
+    Jpsi_t_CStheta[i] = (TH2F*)(accfile[i]->Get("Jpsi_t_CStheta"));
+    Jpsi_l_CStheta[i] = (TH2F*)(accfile[i]->Get("Jpsi_l_CStheta"));
+    B_409[i] = (TH2F*)(accfile[i]->Get("B_409"));
+    B_333[i] = (TH2F*)(accfile[i]->Get("B_333"));
+  }
   
   int nn = 0;
-  for (int ii=1; ii <= Jpsi_non->GetNbinsX(); ii++) {
-    for (int jj=1; jj <= Jpsi_non->GetNbinsY(); jj++) {
-      if ( ii == 2 || jj > 5 ) {  // barrel region excluded
-	nopol[nn]   = Jpsi_non->GetBinContent(ii,jj);
-	trasvHX[nn] = Jpsi_t_HXtheta->GetBinContent(ii,jj);	
-	longHX[nn]  = Jpsi_l_HXtheta->GetBinContent(ii,jj);
-	trasvCS[nn] = Jpsi_t_CStheta->GetBinContent(ii,jj);
-	longCS[nn]  = Jpsi_l_CStheta->GetBinContent(ii,jj);
-	B409[nn]   = B_409->GetBinContent(ii,jj);
-	B333[nn]   = B_333->GetBinContent(ii,jj);
-	cout << "nn " << ii << " " << jj << " " << nn << " " << nopol[nn] << " " << trasvHX[nn] << " " << longHX[nn] << " " << trasvCS[nn] << " " << longCS[nn] << " " << B409[nn] << " " << B333[nn] << endl;
-	nn++;
-      }
+  for (int ii=1; ii <= 3; ii++) {
+    for (int jj=1; jj <= Jpsi_non[ii-1]->GetNbinsY(); jj++) {
+      nopol[nn]   = Jpsi_non[ii-1]->GetBinContent(ii,jj);
+      trasvHX[nn] = Jpsi_t_HXtheta[ii-1]->GetBinContent(ii,jj);	
+      longHX[nn]  = Jpsi_l_HXtheta[ii-1]->GetBinContent(ii,jj);
+      trasvCS[nn] = Jpsi_t_CStheta[ii-1]->GetBinContent(ii,jj);
+      longCS[nn]  = Jpsi_l_CStheta[ii-1]->GetBinContent(ii,jj);
+      B409[nn]   = B_409[ii-1]->GetBinContent(ii,jj);
+      B333[nn]   = B_333[ii-1]->GetBinContent(ii,jj);
+      cout << "nn " << ii << " " << jj << " " << nn << " " << nopol[nn] << " " << trasvHX[nn] << " " << longHX[nn] << " " << trasvCS[nn] << " " << longCS[nn] << " " << B409[nn] << " " << B333[nn] << endl;
+      nn++;
     }
   }
   cout << "OK qui " << endl;  
-
+  
   // B-FRACTION
-  double Bfrac[largeBins] = {0.162,0.257,0.369,0.098,0.112,0.165,0.203,0.331};
-  double errstBfrac[largeBins] = {0.038,0.022,0.027,0.022,0.013,0.019,0.019,0.039};
-  double errsyBfrac[largeBins] = {0.033,0.011,0.014,0.036,0.011,0.010,0.010,0.018};
+  double Bfrac[largeBins] = {0.181,0.261,0.405,0.147,0.178,0.204,0.363,0.057,0.087,0.113,0.138,0.160,0.173,0.233,0.377};
+  double errstBfrac[largeBins] = {0.024,0.015,0.018,0.021,0.017,0.017,0.031,0.021,0.014,0.013,0.014,0.014,0.012,0.016,0.031};
+  double errsyBfrac[largeBins] = {0.019,0.023,0.014,0.034,0.036,0.054,0.028,0.050,0.024,0.022,0.009,0.012,0.009,0.022,0.019};
 
   int tsb = 0;
   for (int i=0; i < largeBins; i++) {
